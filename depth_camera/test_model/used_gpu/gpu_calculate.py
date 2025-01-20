d511afdf8ee6738816035c331a1eaf9bd5793392
teyoon1105 <teyoon1105@gmail.com>
2025-01-20 20:41:11 +0900

    [feat](realsense) : Code that allows gpu to perform parallel calculation of volume

--- depth_camera/test_model/used_gpu/gpu_calculate.py
+++ depth_camera/test_model/used_gpu/gpu_calculate.py
@@ -148,7 +148,7 @@ class DepthVolumeCalculator:
 if __name__ == "__main__":
     # 로그 레벨 설정 (INFO 메시지 비활성화)
     logging.getLogger("ultralytics").setLevel(logging.WARNING)
-    MODEL_PATH = os.path.join(os.getcwd(), 'model', "total_50org_100scaled_10000mix_700_96_a1002_best.pt")
+    MODEL_PATH = os.path.join(os.getcwd(), 'model', "path/your/model.pt")
     ROI_POINTS = [(175, 50), (1055, 690)]
     CLS_NAME_COLOR = {
     '01011001': ('Rice', (255, 0, 255)), # 자주색
